30f4d14d0960f7103215ac1f771613f9a63e5592
davidroman <davidroman1914@gmail.com>
2017-01-12 22:06:59 -0800

    second commit

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.us-west-2.rds.amazonaws.com
+export DB_PRD_HOST=spidertutorial-postgresql.chpn7lwzazu5.us-west-2.rds.amazonaws.com
 export DB_PRD_USER=spidertutorial
-export DB_PRD_PASS=spidertutorial
+export DB_PRD_PASS=f4nnYF0rum
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
