0615959784891c03230afd59b383033382be126e
davidroman <davidroman1914@gmail.com>
2017-01-12 23:45:14 -0800

    next commit

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-'use strict';
-
-const Hapi = require('hapi');
-const constants = require('./src/config/constants');
-
-var routes = require('./src/routes');
-
-
-const server = new Hapi.Server();
-server.connection({port: constants.application.port});
-
-for(var route in routes){
-    server.route(routes[route]);
-}
-
-module.exports = server;
-
-server.start(function(err){
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server  running at:', server.info.uri);
-});
+// This file is just added for convenience so this repository can be
+// directly checked out into a project's deps folder
+module.exports = require('./lib/nodeunit');
